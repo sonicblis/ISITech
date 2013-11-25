1b4189e56caede115aee4b820578b88789a0d693
sonicblis <slashdrew@gmail.com>
2013-11-25 12:59:12 -0500

    Added web project

---
+++ ISITech/ISITech/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ISITech
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ISITech/ISITech/Scripts/app.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module("vote", []);
+app.controller("VotingController", function ($scope) {
+    $scope.Options = [
+        { Name: 'AngularJS', Votes: 8, Presenter: 'Langdon Oliver' },
+        { Name: 'WebAPI', Votes: 3, Presenter: 'Cameron Tabor' },
+        { Name: 'Less', Votes: 2, Presenter: '' },
+        { Name: 'TypeScript', Votes: 3, Presenter: '' }
+    ];
+    $scope.Downvote = function (option) {
+        if (!option.Downvoted) {
+            if (option.Upvoted)
+                option.Votes--;
+            option.Downvoted = true;
+            option.Votes--;
+            option.Upvoted = false;
+        }
+    };
+    $scope.Upvote = function (option) {
+        if (!option.Upvoted) {
+            if (option.Downvoted)
+                option.Votes++;
+            option.Downvoted = false;
+            option.Votes++;
+            option.Upvoted = true;
+        }
+    };
+});
